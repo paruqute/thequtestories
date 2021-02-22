107db4f11a5af3c80012817a79470631f3e1df9c
paruqute <parvathychandran.t@gmail.com>
2021-02-22 12:57:09 +0530

    file name changed

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact Us</a>
+                <a class="nav-link" href="contact.php">Contact Us</a>
               </li>
               
             </ul>
